62e428ee911f3a021bfef89cd636d4a38276ce9c
Anthony Smith <51099384+smitht06@users.noreply.github.com>
2021-09-17 15:54:31 -0400

    added backend

---
+++ frontend/src/components/SiteType.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Card } from 'react-bootstrap'
+
+function SiteType() {
+    return (
+        <div>
+            <Card className='my-3 p-3 rounded'>
+                <Card.Img src='./images/adultfitness.jpeg' variant='top' />
+                <Card.Body>
+                    <strong>Hello world</strong>
+                </Card.Body>
+            </Card>
+        </div>
+    )
+}
+
+export default SiteType
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,11 +1,41 @@
 import React from 'react';
-
+import { Row, Col } from 'react-bootstrap';
 import { Image } from 'react-bootstrap';
+import SiteType from '../components/SiteType';
 
 function HomeScreen() {
 	return (
         <>
 			<Image src='../images/mansion.png' fluid/>
+            <Row>
+            <Col sm={12} md={6} lg={4} xl={3}>
+            <SiteType/>
+            </Col>
+            <Col sm={12} md={6} lg={4} xl={3}>
+            <SiteType/>
+            </Col>
+            <Col sm={12} md={6} lg={4} xl={3}>
+            <SiteType/>
+            </Col>
+            <Col sm={12} md={6} lg={4} xl={3}>
+            <SiteType/>
+            </Col>
+           
+            </Row>
+            <Row>
+            <Col sm={12} md={6} lg={4} xl={3}>
+            <SiteType/>
+            </Col>
+            <Col sm={12} md={6} lg={4} xl={3}>
+            <SiteType/>
+            </Col>
+            <Col sm={12} md={6} lg={4} xl={3}>
+            <SiteType/>
+            </Col>
+            <Col sm={12} md={6} lg={4} xl={3}>
+            <SiteType/>
+            </Col>
+            </Row>
         </>
 	);
 }
