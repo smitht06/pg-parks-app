00fd4f24c9bdec5256422ffd32f882b2d2ee256f
Anthony Smith <51099384+smitht06@users.noreply.github.com>
2021-09-23 09:45:49 -0400

    added site type screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,16 +2,20 @@ import React from 'react';
 import './bootstrap.css';
 import Header from './components/Header';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import {Container} from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import HomeScreen from './screens/HomeScreen';
+import SiteTypeScreen from './screens/SiteTypeScreen'
 
 function App() {
 	return (
 		<Router>
-		<Header/>
+			<Header />
 			<main>
 				<Container>
-					<Route path="/" component={HomeScreen} exact/>
+					<Route path='/' component={HomeScreen} exact />
+				</Container>
+				<Container>
+				<Route path="/sitetype" component={SiteTypeScreen}/>
 				</Container>
 			</main>
 			<footer />
---
+++ frontend/src/screens/SiteTypeScreen.js
@@ -0,0 +1,10 @@
+import { Route } from 'express';
+import React from 'react';
+
+function SiteTypeScreen() {
+	return <div>
+		<h1>HIIII</h1>
+	</div>;
+}
+
+export default SiteTypeScreen;
