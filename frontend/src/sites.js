711d9f3c86011a4e8c6d859e7103df992df10986
Anthony Smith <51099384+smitht06@users.noreply.github.com>
2021-09-24 12:53:44 -0400

    adding sample data and photos

---
+++ frontend/src/sites.js
@@ -0,0 +1,42 @@
+const sites = [
+	{
+		_id: 1,
+		name: 'Calvert Mansion',
+		image: '/images/mansion.png',
+		description:
+			'Calvert mansion is the historical family home of the Calverts. Guided tours and event rentals are available.',
+		type: 'park',
+	},
+	{
+		_id: 2,
+		name: 'College Park Aviation Museum',
+		image: '/images/mansion.png',
+		type: 'Museum',
+	},
+	{
+		_id: 3,
+		name: 'Calvert Mansion',
+		image: '/images/mansion.png',
+		type: 'park',
+	},
+	{
+		_id: 4,
+		name: 'Calvert Mansion',
+		image: '/images/mansion.png',
+		type: 'park',
+	},
+	{
+		_id: 5,
+		name: 'Calvert Mansion',
+		image: '/images/mansion.png',
+		type: 'park',
+	},
+	{
+		_id: 6,
+		name: 'Calvert Mansion',
+		image: '/images/mansion.png',
+		type: 'park',
+	},
+];
+
+export default sites;
